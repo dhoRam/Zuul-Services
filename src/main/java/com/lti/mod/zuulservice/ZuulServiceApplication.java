97821ec8701a04a9c95c95b92f97ae0196389ca7
Karthikeyan <karthikeyan.as@lntinfotech.com>
2020-08-01 11:11:36 +0530

    initial commit

---
+++ src/main/java/com/lti/mod/zuulservice/ZuulServiceApplication.java
@@ -0,0 +1,38 @@
+package com.lti.mod.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+//
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return addCorsMappings(registry) -> { registry.addMapping("/**").allowedOrigins("*");
+//		}
+//
+//	}
+}
+
